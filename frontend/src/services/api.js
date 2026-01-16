65b662e3233998d9595e10fd8c2d03562d7d5458
Pratiti Paul <paulpratiti23@gmail.com>
2026-01-16 14:18:53 +0530

    hosting

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,13 +3,6 @@ import axios from "axios";
 const API = axios.create({
   // baseURL: "http://localhost:5001/api"
   baseURL: import.meta.env.VITE_API_URL
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-});
-
-export default API;
-
 });
 
 API.interceptors.request.use((req) => {
