6708e64145b2f448f6f245fd578b113d661e3520
Pratiti Paul <paulpratiti23@gmail.com>
2026-01-20 22:26:50 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // CORS configuration (allow localhost dev ports and preflight)
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5000","https://medico-sooty-omega.vercel.app"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5000","https://medico-sooty-omega.vercel.app", "/\.vercel\.app$/"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
